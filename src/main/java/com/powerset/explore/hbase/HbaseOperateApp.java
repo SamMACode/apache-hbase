ee149bdd3a6b40bc926e7c73fa37a03e021bd3f2
SamMa <sam_newyork@163.com>
2020-04-13 23:15:26 +0800

    #remove snapshot tag in pom.xml and implement CommandLineRunner interface

--- src/main/java/com/powerset/explore/hbase/HbaseOperateApp.java
+++ src/main/java/com/powerset/explore/hbase/HbaseOperateApp.java
@@ -1,5 +1,6 @@
 package com.powerset.explore.hbase;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,10 +10,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 通过Apache Hbase从csv中导入数据的应用启动类
  */
 @SpringBootApplication
-public class HbaseOperateApp {
+public class HbaseOperateApp implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(HbaseOperateApp.class, args);
+    }
+
+    /**
+     * 可以从java -jar hbase-sample-0.0.1.jar中获取传递的参数信息
+     * @param args
+     * @throws Exception
+     */
+    @Override
+    public void run(String... args) throws Exception {
+
     }
 
 }
